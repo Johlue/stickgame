f8b5241d2a796e97f5f6881443163e695d917564
Johlue <jjohlue@gmail.com>
2019-05-14 15:55:45 +0300

    i don't understand what causes the lag

--- code/GameObjects/Player.cpp
+++ code/GameObjects/Player.cpp
@@ -108,30 +108,18 @@ bool Player::boundaryCollision()
   double shortestDistance = 999999.0;
   double tempDistance = 99999.0;
   CollisionData tempPoint;
-  bool xy = true;   // x and y are positive
-  bool nxy = true;  // x is neg and y is pos
-  bool xny = true;  // x is pos and y is neg
-  bool nxny = true; // x and y are negative
   for(int i = 0; i < objects->size(); i++)
   {
     std::cout << "a";
-    // set all thing to true
-    xy = true; nxy = true; xny = true; nxny = true;
     // run through all boundary type gameobjects
     if((*objects)[i]->getType() == BOUNDARY)
     {
-      // if x+ y+, no (x,y)... etc.
-      if(!(xVel > 0 && yVel > 0)) xy = false;  // no (x, y)
-      if(!(xVel < 0 && yVel > 0)) nxy = false; // no (x+w, y)
-      if(!(xVel > 0 && yVel < 0)) xny = false; // no (x, y+h)
-      if(!(xVel < 0 && yVel < 0)) nxny = false;// no (x+w, y+h)
-
       for(int i2 = 0; i2 < 4; i2++)
       {
-        if (xy && i2 == 0) tempPoint = (*objects)[i]->lineIntersection(x, y, x+xVel, y+yVel, 1, 1, 1, 1);
-        if (nxy && i2 == 1) tempPoint = (*objects)[i]->lineIntersection(x + width, y, x+xVel + width, y+yVel, 1, 1, 1, 1);
-        if (xny && i2 == 2) tempPoint = (*objects)[i]->lineIntersection(x, y + height, x+xVel, y+yVel + height, 1, 1, 1, 1);
-        if (nxny && i2 == 3)
+        if (i2 == 0) tempPoint = (*objects)[i]->lineIntersection(x, y, x+xVel, y+yVel, 1, 1, 1, 1);
+        if (i2 == 1) tempPoint = (*objects)[i]->lineIntersection(x + width, y, x+xVel + width, y+yVel, 1, 1, 1, 1);
+        if (i2 == 2) tempPoint = (*objects)[i]->lineIntersection(x, y + height, x+xVel, y+yVel + height, 1, 1, 1, 1);
+        if (i2 == 3)
         {
           std::cout << "lol";
            tempPoint = (*objects)[i]->lineIntersection(x + width, y + height, x+xVel + width, y+yVel + height, 1, 1, 1, 1);
